e0f10ed6c9222d199322593b8ad100454b9668d2
Jan Dolezel <dolezel.jan@gmail.com>
2018-03-27 09:28:18 +0200

    Fix API tests

--- src/actions/test.js
+++ src/actions/test.js
@@ -1,3 +1,8 @@
 const createWeb = require("../web");
+const actions = require(".");
 
-module.exports = createWeb();
+module.exports = (async () => {
+  const web = await createWeb();
+  web.addRoutes(actions);
+  return web;
+})();

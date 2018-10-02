c0e7b5e6b1d7bcc3abc7d74055485b5341c2c86b
Jan Dolezel <dolezel.jan@gmail.com>
2018-10-02 09:01:48 +0200

    Updated pacakges and client app

---
+++ client-auth/build/precache-manifest.a64fbae8b070ef655bd5b30d64edb518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d55dabc9655ef77206c0",
+    "url": "/auth/static/js/runtime~main.d55dabc9.js"
+  },
+  {
+    "revision": "dcee724c81b4e6595e94",
+    "url": "/auth/static/js/main.dcee724c.chunk.js"
+  },
+  {
+    "revision": "35a02df212a904affa60",
+    "url": "/auth/static/js/1.35a02df2.chunk.js"
+  },
+  {
+    "revision": "dcee724c81b4e6595e94",
+    "url": "/auth/static/css/main.233ac422.chunk.css"
+  },
+  {
+    "revision": "ba88fea58a45f8dafe847819f4c09d6a",
+    "url": "/auth/index.html"
+  }
+];

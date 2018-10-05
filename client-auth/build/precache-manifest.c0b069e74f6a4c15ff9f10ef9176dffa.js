a8d79365c488d6efce6c503b8850e4e249e9058f
Jan Dolezel <dolezel.jan@gmail.com>
2018-10-05 08:26:06 +0200

    Updated dependencies and rebuild client app

---
+++ client-auth/build/precache-manifest.c0b069e74f6a4c15ff9f10ef9176dffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d55dabc9655ef77206c0",
+    "url": "/auth/static/js/runtime~main.d55dabc9.js"
+  },
+  {
+    "revision": "9ee143f2f332a1465a1c",
+    "url": "/auth/static/js/main.9ee143f2.chunk.js"
+  },
+  {
+    "revision": "7d3bfaa0719d90057512",
+    "url": "/auth/static/js/1.7d3bfaa0.chunk.js"
+  },
+  {
+    "revision": "9ee143f2f332a1465a1c",
+    "url": "/auth/static/css/main.64c3c7b1.chunk.css"
+  },
+  {
+    "revision": "210bec7f79e87778326efae7af9aa196",
+    "url": "/auth/index.html"
+  }
+];

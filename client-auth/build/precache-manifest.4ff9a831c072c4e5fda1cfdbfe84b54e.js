878b0df3edaf0b46c006a3354c10c268eb611b77
Jan Dolezel <dolezel.jan@gmail.com>
2019-04-26 10:21:20 +0200

    Updated dependencies, regenerated

---
+++ client-auth/build/precache-manifest.4ff9a831c072c4e5fda1cfdbfe84b54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89071f24ec392847201ff23033020c14",
+    "url": "/auth/index.html"
+  },
+  {
+    "revision": "9c7c5884821b9c51ba0c",
+    "url": "/auth/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/auth/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "9c7c5884821b9c51ba0c",
+    "url": "/auth/static/js/main.284bffa8.chunk.js"
+  },
+  {
+    "revision": "d9022639686edba99362",
+    "url": "/auth/static/js/runtime~main.7372c815.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- client-auth/build/service-worker.js
+++ client-auth/build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/auth/precache-manifest.3bbeba5a4678b4006a328e2ed1f9aa72.js"
+  "/auth/precache-manifest.4ff9a831c072c4e5fda1cfdbfe84b54e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/auth/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/auth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

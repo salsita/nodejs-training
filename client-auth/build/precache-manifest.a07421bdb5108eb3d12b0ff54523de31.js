81d5e1b17615a4a13e0f07ebd551da77c9e6391d
Jan Dolezel <dolezel.jan@gmail.com>
2020-06-15 11:09:12 +0200

    Updated dependencies

--- client-auth/build/precache-manifest.a07421bdb5108eb3d12b0ff54523de31.js
+++ client-auth/build/precache-manifest.a07421bdb5108eb3d12b0ff54523de31.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e1abcff697d4013cf34e54a2697f170",
+    "revision": "37a8a8c2f41dfab593db83b204774455",
     "url": "/auth/index.html"
   },
   {
-    "revision": "c278467325fca9682b72",
+    "revision": "8ddb5b1bcaa82ee08797",
     "url": "/auth/static/css/main.6383cd5b.chunk.css"
   },
   {
-    "revision": "0e2d4a25a8d14ff3445f",
-    "url": "/auth/static/js/2.0ce3e96a.chunk.js"
+    "revision": "8a21cb80c7b061c139fc",
+    "url": "/auth/static/js/2.e0e229ce.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/auth/static/js/2.0ce3e96a.chunk.js.LICENSE.txt"
+    "url": "/auth/static/js/2.e0e229ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c278467325fca9682b72",
-    "url": "/auth/static/js/main.9f63dee1.chunk.js"
+    "revision": "8ddb5b1bcaa82ee08797",
+    "url": "/auth/static/js/main.b9e09886.chunk.js"
   },
   {
     "revision": "c273344ff57e6595f383",
